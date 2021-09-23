8ba1af82824fda15d86f0850f98506856478ef22
rpm5735 <rpm5735@psu.edu>
2021-09-23 13:36:24 -0400

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,203 @@
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">namazon</a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="singin.html">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          
+          <div className="card">
+            <a href="product.html">
+          
+              <img className="medium" src="./images/samsung85.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h4>Samsung 85 Inch</h4>
+              </a>
+              <div className="rating">
+                <span> <i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star-half-o"></i></span>
+              </div>
+              <div className="price">$1899</div>
+            </div>
+          </div>
+          
+          <div className="card">
+            <a href="product.html">
+              
+              <img className="medium" src="./images/sony85.webp" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h4>Sony 85 Inch</h4>
+              </a>
+              <div className="rating">
+                <span> <i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star-o"></i></span>
+              </div>
+              <div className="price">$1999</div>
+            </div>
+          </div>
+          
+          <div className="card">
+            <a href="product.html">
+              
+              <img className="medium" src="./images/tcl85.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h4>TCL 85 Inch</h4>
+              </a>
+              <div className="rating">
+                <span> <i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star-half-o"></i></span>
+              </div>
+              <div className="price">$1699</div>
+            </div>
+          </div>
+          
+          <div className="card">
+            <a href="product.html">
+              
+              <img className="medium" src="./images/lg65.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h4>LG 65 Inch</h4>
+              </a>
+              <div className="rating">
+                <span> <i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star-o"></i></span>
+              </div>
+              <div className="price">$995</div>
+            </div>
+          </div>
+          
+          <div className="card">
+            <a href="product.html">
+              
+              <img className="medium" src="./images/samsung65.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h4>Samsung 65 Inch</h4>
+              </a>
+              <div className="rating">
+                <span> <i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+              </div>
+              <div className="price">$899</div>
+            </div>
+          </div>
+          
+          <div className="card">
+            <a href="product.html">
+              
+              <img className="medium" src="./images/vizio65.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h4>Vizio 65 Inch</h4>
+              </a>
+              <div className="rating">
+                <span> <i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star-half-o"></i></span>
+              </div>
+              <div className="price">$799</div>
+            </div>
+          </div>
+      
+          <div className="card">
+            <a href="product.html">
+              
+              <img className="medium" src="./images/insignia43.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h4>Insignia 43 Inch</h4>
+              </a>
+              <div className="rating">
+                <span> <i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star-half-o"></i></span>
+              </div>
+              <div className="price">$399</div>
+            </div>
+          </div>
+          
+          <div className="card">
+            <a href="product.html">
+            
+              <img className="medium" src="./images/samsung43.webp" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h4>Samsung 43 Inch</h4>
+              </a>
+              <div className="rating">
+                <span> <i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star-half-o"></i></span>
+              </div>
+              <div className="price">$599</div>
+            </div>
+          </div>
+          
+          <div className="card">
+            <a href="product.html">
+              
+              <img className="medium" src="./images/toshiba43.jpg" alt="product" />
+            </a>
+            <div classNameName="card-body">
+              <a href="product.html">
+                <h4>Toshiba 43 Inch</h4>
+              </a>
+              <div className="rating">
+                <span> <i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star-half-o"></i></span>
+              </div>
+              <div className="price">$499</div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className="row center">All Rights Reserved</footer>
+    </div>
+
+  );
+}
+
+export default App;
